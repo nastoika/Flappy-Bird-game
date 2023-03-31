d0f709ae064fb68b6a7e5a84f3992187b69c9f56
nastoika <nastya.gorlachyova@mail.ru@>
2023-03-31 16:27:08 +0300

    add press button

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const bg = new Image();
 const fg = new Image();
 const pipeUp = new Image();
 const pipeBottom = new Image();
+
 const gap = 90;
 
 bird.src = 'img/bird.png';
@@ -14,12 +15,26 @@ fg.src = 'img/fg.png';
 pipeUp.src = 'img/pipeUp.png';
 pipeBottom.src = 'img/pipeBottom.png';
 
+//press button
+document.addEventListener('keydown', moveUp);
+function moveUp() {
+  yPos -= 20;
+}
+
+//bird position
+let xPos = 10;
+let yPos = 150;
+let grav = 1;
+
 function draw() {
   ctx.drawImage(bg, 0, 0);
   ctx.drawImage(pipeUp, 100, 0);
   ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
   ctx.drawImage(fg, 0, canvas.height - fg.height);
-  ctx.drawImage(bird, 10, 150);
+  ctx.drawImage(bird, xPos, yPos);
+
+  yPos += grav;
+  requestAnimationFrame(draw);
 }
 
 pipeBottom.onload = draw;
